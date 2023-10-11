9161f9fe45c282a4ede462f1a411dc08ec972099
TonyDN98 <107117383+TonyDN98@users.noreply.github.com>
2023-10-11 11:14:53 +0000

    Js Odin Fundamentals 1

--- odin-p/js-fudamentals/01-fundamental/javascript.js
+++ odin-p/js-fudamentals/01-fundamental/javascript.js
@@ -2,18 +2,25 @@ console.log("from js file");
 
 // Variable declaration with let and const for NOW.
 
-console.log(23+97);
-console.log(1+2+3+4+5+5);
-console.log((4+6+9) / 77);
+//Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+console.log("23+97=", 23+97);
+//Add a sequence of 6 different numbers together.
+console.log("1+2+3+4+5+5=",1+2+3+4+5+5);
+//Print the solution to the following equation: (4 + 6 + 9) / 77
+console.log("(4+6+9) / 77=",(4+6+9) / 77);
 
 let a = 10;
-console.log(a);
+console.log("a="+a);
+console.log("9*a="+9*a);
 
-console.log(9*a);
+let b = 7* a;
+console.log("b= "+b);
 
-const  max = 57
-let actual = max -17
-let percentage = actual / max
+// Declare a constant variable max with the value 57
+const  MAX = 57
+let actual = MAX -17
+let percentage = actual / MAX
+console.log("Percentage= ", percentage);
 
 // == equal and === not equal
 
